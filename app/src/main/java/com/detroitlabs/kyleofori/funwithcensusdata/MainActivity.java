e2b80f84ac28b59d38db1bfac7a220f7576642bb
Kyle Ofori <kyle.ofori@detroitlabs.com>
2015-06-19 16:22:54 -0400

    Adding code for intents and removing transparency SeekBar

--- app/src/main/java/com/detroitlabs/kyleofori/funwithcensusdata/MainActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/funwithcensusdata/MainActivity.java
@@ -1,10 +1,11 @@
 package com.detroitlabs.kyleofori.funwithcensusdata;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.SeekBar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,34 +21,45 @@ import com.google.android.gms.maps.model.PolygonOptions;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
-    private static final int TRANSPARENCY_MAX = 100;
-    private static final LatLng NEWARK = new LatLng(40.714086, -74.228697);
+public class MainActivity extends AppCompatActivity implements BoundaryDataReceiver.Receiver {
 
+    public BoundaryDataReceiver boundaryDataReceiver;
+
+    private static final LatLng NEWARK = new LatLng(40.714086, -74.228697);
     private final List<BitmapDescriptor> mImages = new ArrayList<BitmapDescriptor>();
 
     private GoogleMap mMap;
     private GroundOverlay mGroundOverlay;
-    private SeekBar mTransparencyBar;
 
     private int mCurrentEntry = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        boundaryDataReceiver = new BoundaryDataReceiver(new Handler());
+        boundaryDataReceiver.setReceiver(this);
+
         setContentView(R.layout.activity_main);
 
-        mTransparencyBar = (SeekBar) findViewById(R.id.transparencySeekBar);
-        mTransparencyBar.setMax(TRANSPARENCY_MAX);
-        mTransparencyBar.setProgress(0);
-
         setUpMapIfNeeded();
+
+        final Intent intent = new Intent(Intent.ACTION_SYNC, null, this, QueryService.class);
+        intent.putExtra("receiver", boundaryDataReceiver);
+        intent.putExtra("command", "query");
+        startService(intent);
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        boundaryDataReceiver.setReceiver(null);
     }
 
     private void setUpMapIfNeeded() {
@@ -72,8 +84,6 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
                 .image(mImages.get(mCurrentEntry)).anchor(0, 1)
                 .position(NEWARK, 8600f, 6500f));
 
-        mTransparencyBar.setOnSeekBarChangeListener(this);
-
         Polygon polygon = mMap.addPolygon(new PolygonOptions()
                 .add(new LatLng(35, -90), new LatLng(36, -89), new LatLng(36, -81), new LatLng(35, -84))
                 .strokeColor(Color.GREEN)
@@ -81,23 +91,21 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     }
 
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-    }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-    }
-
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        if (mGroundOverlay != null) {
-            mGroundOverlay.setTransparency((float) progress / (float) TRANSPARENCY_MAX);
-        }
-    }
-
     public void switchImage(View view) {
         mCurrentEntry = (mCurrentEntry + 1) % mImages.size();
         mGroundOverlay.setImage(mImages.get(mCurrentEntry));
+    }
+
+    @Override
+    public void onReceiveResult(int resultCode, Bundle resultData) {
+        switch (resultCode) {
+            case QueryService.STATUS_RUNNING:
+                break;
+            case QueryService.STATUS_FINISHED:
+                List results = resultData.getParcelableArrayList("results");
+                break;
+            case QueryService.STATUS_ERROR:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/detroitlabs/kyleofori/funwithcensusdata/QueryService.java
@@ -0,0 +1,42 @@
+package com.detroitlabs.kyleofori.funwithcensusdata;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.os.ResultReceiver;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kyleofori on 6/19/15.
+ */
+
+public class QueryService extends IntentService {
+
+    final static int STATUS_FINISHED = 1;
+    final static int STATUS_RUNNING = 0;
+    final static int STATUS_ERROR = -1;
+    private ArrayList<? extends Parcelable> results = new ArrayList();
+
+    public QueryService(String name) {
+        super(name);
+    }
+
+    protected void onHandleIntent(Intent intent) {
+        final ResultReceiver receiver = intent.getParcelableExtra("receiver");
+        String command = intent.getStringExtra("command");
+        Bundle b = new Bundle();
+        if(command.equals("query")) {
+            receiver.send(STATUS_RUNNING, Bundle.EMPTY);
+            try {
+                // get some data or something
+                b.putParcelableArrayList("results", results);
+                receiver.send(STATUS_FINISHED, b);
+            } catch(Exception e) {
+                b.putString(Intent.EXTRA_TEXT, e.toString());
+                receiver.send(STATUS_ERROR, b);
+            }
+        }
+    }
+}
