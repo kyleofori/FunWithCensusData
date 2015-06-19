e9d9e0890f9d5a6a521875c5cd65de660614989e
Kyle Ofori <kyle.ofori@detroitlabs.com>
2015-06-19 12:23:29 -0400

    Mimicking Google Maps sample app

--- app/src/main/java/com/detroitlabs/kyleofori/funwithcensusdata/MainActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/funwithcensusdata/MainActivity.java
@@ -1,37 +1,95 @@
 package com.detroitlabs.kyleofori.funwithcensusdata;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+
+    private static final int TRANSPARENCY_MAX = 100;
+    private static final LatLng NEWARK = new LatLng(40.714086, -74.228697);
+
+    private final List<BitmapDescriptor> mImages = new ArrayList<BitmapDescriptor>();
+
+    private GoogleMap mMap;
+    private GroundOverlay mGroundOverlay;
+    private SeekBar mTransparencyBar;
+
+    private int mCurrentEntry = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTransparencyBar = (SeekBar) findViewById(R.id.transparencySeekBar);
+        mTransparencyBar.setMax(TRANSPARENCY_MAX);
+        mTransparencyBar.setProgress(0);
+
+        setUpMapIfNeeded();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            if (mMap != null) {
+                setUpMap();
+            }
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void setUpMap() {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NEWARK, 11));
+
+        mImages.clear();
+        mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.newark_nj_1922));
+        mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.newark_prudential_sunny));
+
+        mCurrentEntry = 0;
+        mGroundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+                .image(mImages.get(mCurrentEntry)).anchor(0, 1)
+                .position(NEWARK, 8600f, 6500f));
+
+        mTransparencyBar.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (mGroundOverlay != null) {
+            mGroundOverlay.setTransparency((float) progress / (float) TRANSPARENCY_MAX);
+        }
+    }
+
+    public void switchImage(View view) {
+        mCurrentEntry = (mCurrentEntry + 1) % mImages.size();
+        mGroundOverlay.setImage(mImages.get(mCurrentEntry));
     }
 }
