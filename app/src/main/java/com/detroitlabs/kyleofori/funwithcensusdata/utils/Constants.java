e8a251c05d939db76e4ab7c738de52624a5fe23e
Kyle Ofori <kyle.ofori@detroitlabs.com>
2016-06-07 15:15:22 -0400

    Delete all state constants

--- app/src/main/java/com/detroitlabs/kyleofori/funwithcensusdata/utils/Constants.java
+++ app/src/main/java/com/detroitlabs/kyleofori/funwithcensusdata/utils/Constants.java
@@ -7,14 +7,6 @@ public class Constants {
     public static final String ERIC_CLST_API_BASE_URL = "http://eric.clst.org";
     public static final String GOOGLE_MAPS_API_BASE_URL = "http://maps.googleapis.com/maps/api";
 
-//    USA political units
-    public static final String AK = "Alaska";
-    public static final String CA = "California";
-    public static final String DC = "District of Columbia";
-    public static final String IA = "Iowa";
-    public static final String MI = "Michigan";
-    public static final String OH = "Ohio";
-
 //    other needed strings
     public static final String AA_LEVEL_1 = "administrative_area_level_1";
     public static final String POLYGON = "Polygon";
